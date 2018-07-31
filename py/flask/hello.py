e57e38cb2cd472c1d92f93e1d6341c8adf589fd5
Klaus Eckelt <Klaus.Eckelt@jku.at>
2018-07-31 09:49:30 +0200

    Logging in flask app

--- py/flask/hello.py
+++ py/flask/hello.py
@@ -1,6 +1,11 @@
+import logging
 from flask import Flask
+
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+_log = logging.getLogger(__name__)
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    _log.info('hello_world() called.')
     return 'Hello, World!'
